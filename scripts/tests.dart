c91f0d5d90cd6dc28d869eae32554777cf2c8223
Shane Ludwig <shane.ludwig01@gmail.com>
2025-01-14 15:48:47 -0800

    Assignment2-1 with temperature, shortForecast, detailedForecast

--- scripts/tests.dart
+++ scripts/tests.dart
@@ -6,12 +6,15 @@ void main() async {
   String pointsUrl = "https://api.weather.gov/points/44.058,-121.31";
   Map<String, dynamic> pointsJsonData = await getJsonFromUrl(pointsUrl);
 
-
   String forecastUrl = pointsJsonData["properties"]["forecast"];
   String forecastHourlyUrl = pointsJsonData["properties"]["forecastHourly"];
 
   Map<String, dynamic> forecastJsonData = await getJsonFromUrl(forecastUrl);
-  Map<String, dynamic> forecastHourlyJsonData = await getJsonFromUrl(forecastHourlyUrl);
+  Map<String, dynamic> forecastHourlyJsonData =
+      await getJsonFromUrl(forecastHourlyUrl);
+
+  processForecasts(forecastJsonData["properties"]["periods"]);
+  processForecasts(forecastHourlyJsonData["properties"]["periods"]);
 
   return;
 }
@@ -21,16 +24,24 @@ Future<Map<String, dynamic>> getJsonFromUrl(String url) async {
   return convert.jsonDecode(r.body);
 }
 
-void processForecasts(Map<String, dynamic> forecasts){
+void processForecasts(List<dynamic> forecasts) {
+  for (var forecast in forecasts) {
+    processForecast(forecast);
+  }
   // TODO: pass the array of forcasts in from main
   // For loop through the forecasts and process each forecast with the
   // processForecast function below
 }
 
-void processForecast(Map<String, dynamic> forecast){
+void processForecast(Map<String, dynamic> forecast) {
+  int bidailyTemperature = forecast["temperature"];
+  String bidailyShortForecast = forecast["shortForecast"];
+  String bidailydetailedForecast = forecast["detailedForecast"];
+  // if (forecast["dewpoint"] != null) {
+  //   int dewpoint = forecast["dewpoint"]["value"];
+  // }
   // TODO: Pass a forecast entry (either hourly or bidaily), and extract
   // The proper values that will be useful. i.e. temperature, shortForecast, longForecast
   // for now, don't return anything, just assign values for each
   // i.e. String shortForcast = "";
-
 }
