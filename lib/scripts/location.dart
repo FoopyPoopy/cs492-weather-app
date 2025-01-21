34e2f6e42da9acea82532cdf2c15f1cdfd4dc68e
Shane Ludwig <Shane.ludwig01@gmail.com>
2025-01-21 15:34:55 -0800

    Assignment3-1 should be good

--- lib/scripts/location.dart
+++ lib/scripts/location.dart
@@ -1,20 +1,33 @@
 import 'package:geocoding/geocoding.dart' as geocoding;
 
 // TODO: Create a new class called Location
+class Location {
+  final String? city;
+  final String? state;
+  final String? zip;
+  final double? lat;
+  final double? lon;
+
+  Location(
+      {required this.city,
+      required this.state,
+      required this.zip,
+      required this.lat,
+      required this.lon});
+}
 // This class should store the values of our location:
 // city, state, zip, latitude, longitude
 // use appropriate data types as well as appropriate null safing for city state zip
 // you do not need to create a factory, but you do need the basic contstructor
 // use the forecast class as a template to help you
 
-
 // TODO: set the type of this function to Future<Location?>
 // create a Location object from the lat, lon, city, state, and zip
 // return the Location if it's found, null if it's not found
 
-void getLocationFromAddress(String rawCity, String rawState, String rawZip) async {
+Future<Location?> getLocationFromAddress(String rawCity, String rawState, String rawZip) async {
   String address = '$rawCity $rawState $rawZip';
-  try{ 
+  try {
     List<geocoding.Location> locations = await geocoding.locationFromAddress(address);
     double lat = locations[0].latitude;
     double lon = locations[0].longitude;
@@ -22,9 +35,8 @@ void getLocationFromAddress(String rawCity, String rawState, String rawZip) asyn
     String? state = placemarks[0].administrativeArea;
     String? city = placemarks[0].locality;
     String? zip = placemarks[0].postalCode;
+    return Location(city: city, state: state, zip: zip, lat: lat, lon: lon);
   } on geocoding.NoResultFoundException {
     return null;
   }
-
-  return;
 }
--- lib/scripts/tests.dart
+++ lib/scripts/tests.dart
@@ -6,18 +6,27 @@ import 'location.dart' as location;
 // }
 
 void testLocation() async {
+  List<Map<String, String>> locations = [
+    {"city": "Bend", "state": "OR", "zip": "97702"}, // Bend, OR
+    {"city": "Seattle", "state": "WA", "zip": "98102"}, // Seattle, WA
+    {"city": "San Jose", "state": "CA", "zip": "95123"}, // San Jose, CA
+    {"city": "San Diego", "state": "CA", "zip": "92109"}, // San Diego, CA
+    {"city": "Honolulu", "state": "HI", "zip": "96795"}, // Honolulu, HI
+    {"city": "Bloomington", "state": "MN", "zip": "55122"} // Bloomington, MN
+  ];
 
   // TODO: Create a list of Map<String, String>
   // Add several (at least 5) city, state, zip Map<String, String> to the list
   // iterate through the list, calling location.getLocationFromAddress function for each iteration
   // passing in the city, state, and zip.
   // Debug with a breakpoint after the return (you can use a placeholder like print("test") for your breakpoint)
-  // Check to ensure each location returns as expected through debugging. 
-
-  location.getLocationFromAddress("oijeqofwkjfla", "asdfsd", "98839829382");
-
+  // Check to ensure each location returns as expected through debugging.
+  List<Future<location.Location?>> locs = [];
+  for (Map<String, String> loc in locations) {
+    locs.add(location.getLocationFromAddress(loc["city"]!, loc["state"]!, loc["zip"]!));
+  }
+  print("this sucks");
 }
-
 
 void testForecast() async {
 // testing with Bend, OR coordinates
@@ -31,8 +40,9 @@ void testForecast() async {
     [35.0844, -106.65]
   ];
 
-  for (List<double> coord in coords){
+  for (List<double> coord in coords) {
     List<forecast.Forecast> forecasts = await forecast.getForecastFromPoints(coord[0], coord[1]);
-    List<forecast.Forecast> forecastsHourly = await forecast.getForecastHourlyFromPoints(coord[0],coord[1]);
+    List<forecast.Forecast> forecastsHourly =
+        await forecast.getForecastHourlyFromPoints(coord[0], coord[1]);
   }
 }
